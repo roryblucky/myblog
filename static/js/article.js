9378572558e5ab9d9ee8e25bac2c8e722bf1d853
Rory Gao <roryblucky@gmail.com>
2016-01-04 15:48:43 +0800

    add subContent function

--- static/js/article.js
+++ static/js/article.js
@@ -2,5 +2,16 @@
  * Created by RoryGao on 15/12/16.
  */
 function convertArticle2HTML(id, data) {
-    $('#article-' + id).html(markdown.toHTML(data))
+    $('#article-' + id).html(subContent(markdown.toHTML(data), 200))
+}
+
+function subContent(data, leng) {
+    console.log(data.length);
+    if (data.length == leng || data.length < leng) {
+        return data;
+    }
+    var newData = data.substring(0, leng);
+    console.log(newData);
+    newData = newData + ".....";
+    return newData
 }
